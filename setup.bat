877b61c5628702c4885ffb00f28176db9f4eb5e0
sfswift <captnswift@gmail.com>
2018-07-22 00:59:52 -0700

    Updates to setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,17 @@
+:: inital setup for the pixel_upper project
+:: 
+
+:: install virutalenv for the environment
+pip install virtualenv
+
+:: create the virtual environment
 py -m virtualenv env
+
+:: install necessary packages to env
 pip install -r requirements.txt
+
+:: input the github user email
 git config user.email "user@domain.com"
+
+:: verify the user email was changed
 git config user.email
