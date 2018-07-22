3e598a1ef8fe3164ce332aecb212e42b9a59c95b
sfswift <captnswift@gmail.com>
2018-07-22 00:53:36 -0700

    Updates to setup docs

--- setup.bat
+++ setup.bat
@@ -1,6 +1,4 @@
-
-pip install virtualenv
 py -m virtualenv env
 pip install -r requirements.txt
-git config user.email "connerballew@hotmail.com"
+git config user.email "user@domain.com"
 git config user.email
