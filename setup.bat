6aa91178cfb608db7a5a30b5a9421806cbfc786c
Conner Ballew <connerballew@hotmail.com>
2018-07-22 00:08:27 -0700

    Updated environment setup

--- setup.bat
+++ setup.bat
@@ -1,4 +1,6 @@
+
+pip install virtualenv
 py -m virtualenv env
 pip install -r requirements.txt
-git config user.email "user@domain.com"
+git config user.email "connerballew@hotmail.com"
 git config user.email
